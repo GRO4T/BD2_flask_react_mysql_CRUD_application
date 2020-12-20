25161c7138df019c21c995d6edbb4e7a1e0a6cb9
Damian Kolaska <d.kolaska@stud.elka.pw.edu.pl>
2020-12-20 01:51:14 +0100

    Generated data for rest of the tables

--- initializer/app/generators/hierarchy.py
+++ initializer/app/generators/hierarchy.py
@@ -3,29 +3,30 @@ departments = ["Dział HR", "Dział sprzedaży", "Dział reklamy", "Dział rozwo
 number_of_groups = 10 * len(departments)
 number_of_teams = 10 * number_of_groups
 
-def generate_hierarchy(data):
-    num_of_employees = len(data)
+def generate_hierarchy(employees):
+    company = []
+    num_of_employees = len(employees)
     start = 0
     end = len(director_roles)
-    start, end = create_board_of_directors(data, start, end)
-    start, end = create_departments(data, start, end)
-    start, end = create_groups(data, start, end)
-    start, end = create_teams(data, start, end)
-    assign_to_teams(data, start, num_of_employees)
-    return data
+    start, end = create_board_of_directors(company, employees, start, end)
+    start, end = create_departments(company, employees, start, end)
+    start, end = create_groups(company, employees, start, end)
+    start, end = create_teams(company, employees, start, end)
+    assign_to_teams(employees, start, num_of_employees)
+    return company
 
-def create_board_of_directors(data, start, end):
+def create_board_of_directors(company, employees, start, end):
     for i in range(start, end): # create board of directors
         board_member = {
             "nazwa_stanowiska": director_roles[i],
             "pracownik_id": i+1
         }
-        data.append([("zarzad", board_member)])
+        company.append([("zarzad", board_member)])
     start = start + len(director_roles)
     end = end + len(departments)
     return start, end
 
-def create_departments(data, start, end):
+def create_departments(company, employees, start, end):
     for i in range(start, end): # create company departments
         dep_number = i - len(director_roles)
         dep_name = departments[dep_number]
@@ -37,13 +38,13 @@ def create_departments(data, start, end):
             "zarzad_id": board_member_id,
             "kierownik_id": i+1
         }
-        data.append([("dzial", dep)])
-        data[i][1][1]["pracownik_id"] = board_member_id
+        company.append([("dzial", dep)])
+        employees[i][1][1]["pracownik_id"] = board_member_id
     start = start + len(departments)
     end = end + number_of_groups
     return start, end
 
-def create_groups(data, start, end):
+def create_groups(company, employees, start, end):
     for i in range(start, end): # create groups
         dep_id = (i % len(departments)) + 1
         group = {
@@ -51,13 +52,13 @@ def create_groups(data, start, end):
             "dzial_id": dep_id,
             "kierownik_id": i+1
         }
-        data.append([("grupa", group)])
-        data[i][1][1]["pracownik_id"] = start + dep_id - len(departments)
+        company.append([("grupa", group)])
+        employees[i][1][1]["pracownik_id"] = start + dep_id - len(departments)
     start = start + number_of_groups
     end = end + number_of_teams
     return start, end
 
-def create_teams(data, start, end):
+def create_teams(company, employees, start, end):
     for i in range(start, end): # create teams
         group_id = (i % number_of_groups) + 1
         team = {
@@ -65,13 +66,13 @@ def create_teams(data, start, end):
             "grupa_id": group_id,
             "kierownik_id": i+1
         }
-        data.append([("zespol", team)])
-        data[i][1][1]["pracownik_id"] = start + group_id - number_of_groups
+        company.append([("zespol", team)])
+        employees[i][1][1]["pracownik_id"] = start + group_id - number_of_groups
     start = start + number_of_teams
     return start, end
 
-def assign_to_teams(data, start, end):
+def assign_to_teams(employees, start, end):
     for i in range(start, end):
         team_id = (i % number_of_teams) + 1
-        data[i][1][1]["pracownik_id"] = start + team_id - number_of_teams
+        employees[i][1][1]["pracownik_id"] = start + team_id - number_of_teams
 
