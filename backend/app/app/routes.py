ebd9001505272a10a53bd7061890af1c51db3429
Damian Kolaska <d.kolaska@stud.elka.pw.edu.pl>
2021-01-10 23:56:56 +0100

    pydantic serialization

--- backend/app/app/routes.py
+++ backend/app/app/routes.py
@@ -1,8 +1,12 @@
 from app import app
 from app import db
 from app import session
-from app.orm import *
+from app.orm import Dzial, Pracownik
+from flask import jsonify
+from pydantic import BaseModel
+from pydantic_sqlalchemy import sqlalchemy_to_pydantic
 
+PydanticPracownik = sqlalchemy_to_pydantic(Pracownik)
 
 @app.route('/api/test')
 def index():
@@ -13,3 +17,7 @@ def index():
 def dzial():
     departments =[ (i.id, i.nazwa) for i in session.query(Dzial).all() ]
     return {'res': departments}
+
+@app.route('/api/pracownik')
+def pracownik():
+    return jsonify([PydanticPracownik.from_orm(prac).json() for prac in session.query(Pracownik).all()])
